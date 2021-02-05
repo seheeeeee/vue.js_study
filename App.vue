5d9c61adccd4205868b5c2e3418681bd24eddff5
seheeeeee <71268946+seheeeeee@users.noreply.github.com>
2021-02-05 17:40:25 +0900

    Vue CLI 생성 및 파일 변경

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- 최상위 element는 하나만 있어야함 -->
+    {{str}}
+  </div>
+</template>
+
+<script>
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
+export default {
+  //컴포넌트 재사용을 위해 다른 컴포넌트의 data를 참조할 수 없기 때문에 객체로 저장할 수 없고 function return을 사용해야함
+  data: function(){
+    return{
+      str: 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
